78d58775a0960bdcd3f39ed7089c50081c64d552
Andrew Kanieski <accounts@andrewkanieski.com>
2022-07-24 13:36:15 -0400

    Added `write_bytes_with_value`  and `swap_endianness` for BinaryWriter (#36)
    * Added write_bytes_with_value
    
    * Adding swap_endiannes to writer

--- src/lib.rs
+++ src/lib.rs
@@ -362,6 +362,22 @@ impl<'a> BinaryWriter<'a> {
     pub fn write_bytes<B: AsRef<[u8]>>(&mut self, data: B) -> Result<usize> {
         Ok(self.stream.write(data.as_ref())?)
     }
+
+    /// Write a byte buffer to the stream.
+    pub fn write_bytes_with_value(&mut self, count: usize, fill_value: u8) -> Result<usize> {
+        let mut buff = Vec::with_capacity(count) as Vec<u8>;
+        buff.resize(count, fill_value);
+        Ok(self.write_bytes(buff)?)
+    }
+
+    /// Swap endianness to allow for reversing the writing mid stream
+    pub fn swap_endianness(&mut self) {
+        if self.endian == Endian::Big {
+            self.endian = Endian::Little;
+        } else {
+            self.endian = Endian::Big;
+        }
+    }
 }
 
 /// Trait for encoding to binary.
